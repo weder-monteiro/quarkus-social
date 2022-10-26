334281a3fe28e6930e2bd82c4c6839b3c1e2dffd
Weder <weder_monteiro@goodyear.com>
2022-10-26 10:20:45 -0300

    Recebendo e devolvendo objetos JSON

--- src/main/java/io/github/wedermonteiro/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/wedermonteiro/quarkussocial/rest/UserResource.java
@@ -1,17 +1,27 @@
 package io.github.wedermonteiro.quarkussocial.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.github.wedermonteiro.quarkussocial.rest.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     
     @POST
     public Response createUser(CreateUserRequest userRequest) {
-        return Response.ok().build();
+        return Response.ok(userRequest).build();
     }
 
+    @GET
+    public Response listAllUsers() {
+        return Response.ok().build();
+    }
 }
