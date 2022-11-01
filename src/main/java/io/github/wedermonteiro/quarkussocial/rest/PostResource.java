9feb4156817e082b71e0437f174f27b14fd49a28
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-11-01 10:27:21 -0300

    Restrigindo o acesso aos posts

--- src/main/java/io/github/wedermonteiro/quarkussocial/rest/PostResource.java
+++ src/main/java/io/github/wedermonteiro/quarkussocial/rest/PostResource.java
@@ -6,15 +6,18 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import io.github.wedermonteiro.quarkussocial.rest.domain.model.Post;
 import io.github.wedermonteiro.quarkussocial.rest.domain.model.User;
+import io.github.wedermonteiro.quarkussocial.rest.domain.repository.FollowerRepository;
 import io.github.wedermonteiro.quarkussocial.rest.domain.repository.PostRepository;
 import io.github.wedermonteiro.quarkussocial.rest.domain.repository.UserRepository;
 import io.github.wedermonteiro.quarkussocial.rest.dto.CreatePostRequest;
@@ -30,11 +33,17 @@ public class PostResource {
 
     private UserRepository userRepository;
     private PostRepository postRepository;
+    private FollowerRepository followerRepository;
     
     @Inject
-    public void UserResource(UserRepository userRepository, PostRepository postRepository) {
+    public void UserResource(
+        UserRepository userRepository, 
+        PostRepository postRepository,
+        FollowerRepository followerRepository
+    ) {
         this.userRepository = userRepository;
         this.postRepository = postRepository;
+        this.followerRepository = followerRepository;
     };
 
     @POST
@@ -46,7 +55,7 @@ public class PostResource {
         User user = userRepository.findById(userId);
 
         if(user == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(Status.NOT_FOUND).build();
         }
 
         Post post = new Post();
@@ -55,15 +64,43 @@ public class PostResource {
         
         postRepository.persist(post);
 
-        return Response.status(Response.Status.CREATED).build(); 
+        return Response.status(Status.CREATED).build(); 
     }
 
     @GET
-    public Response listById(@PathParam("userId") Long userId) {
+    public Response list(
+        @PathParam("userId") Long userId, 
+        @HeaderParam("followerId") Long followerId
+    ) {
         User user = userRepository.findById(userId);
 
         if(user == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        if(followerId == null) {
+            return Response
+                .status(Status.BAD_REQUEST)
+                .entity("You forgot the header followerId")
+                .build();
+        }
+
+        User follower = userRepository.findById(followerId);
+
+        if(follower == null) {
+            return Response
+                .status(Status.BAD_REQUEST)
+                .entity("Inexistent followerId")
+                .build();
+        }
+
+        boolean follows = followerRepository.follows(follower, user);
+
+        if(!follows) {
+            return Response
+                .status(Status.FORBIDDEN)
+                .entity("You can´t see these posts")
+                .build();
         }
 
         PanacheQuery<Post> query = postRepository.find(
